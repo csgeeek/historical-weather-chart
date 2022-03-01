34bd677518bbc6041cba9150205291e242c3d2eb
csg33k <balajiyaswanth.v@gmail.com>
2022-03-02 00:33:07 +0530

    fixed chart

--- script.js
+++ script.js
@@ -3,23 +3,22 @@ const myform = document.querySelector('#myform');
 const prepY = async (latv, lonv) => {
     const ydata = [];
     const APPID = config.SECRET_API_KEY;    
-        // api.openweathermap.org/data/2.5/weather?lat=34&lon=35&appid=
-        
+        if(myChart != undefined){
+            myChart.destroy();
+        }
         for(let i = 1; i <= 12; i++){
             let URL = `https://history.openweathermap.org/data/2.5/aggregated/month?month=${i}&lat=${latv}&lon=${lonv}&appid=${APPID}`;
             const res = await fetch(URL);
             const data = await res.json();
-            const temp = await data["result"]["temp"]["record_max"];
+            const temp = await data["result"]["temp"]["median"];
             ydata.push(parseFloat(temp) - 273);
         }
     return ydata;
 }
 
-// let myychart;
 myform.addEventListener('submit',async (e) => {
     e.preventDefault();
     console.log('Clicked');
-    // document.querySelector(".chart-container").innerHTML = '<canvas id="chart"></canvas>';
     const lat = document.querySelector('#lat');
     const lon = document.querySelector('#lon');
     const latv = lat.value;
@@ -27,43 +26,18 @@ myform.addEventListener('submit',async (e) => {
     if((latv >= -90 && latv <= 90) && (lonv >= -180 && lonv <= 180)) {   
         const ydata = await prepY(latv, lonv);
         await chartIt(ydata);
-        // destroyChart();
-        // console.log(ydata2);
     }
 });
 
-// console.log(ydata2);
-// btn.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     console.log('e.value');
-// });
 
 const xlabels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-// const temperatures = async () => {
-//     const res = await fetch('/dataset.csv');
-//     const data = await res.text();
 
-//     const table = data.split('\n');
-//     // console.log(table);
-//     table.forEach( row => {
-//         const col = row.split(',');
-//         const year = col[0];
-//         xlabels.push(year);
-//         const temperature = col[1];
-//         ydata.push(parseFloat(temperature) + 14);
-//         // console.log(year, temperature);
-//     })
-// }
-
-
+let myChart;
 
 const ctx = document.getElementById('chart').getContext('2d');
 const chartIt = async (ydata) => {
-    // await temperatures();
-    if(window.myChart != undefined)
-        window.myChart.destroy();
-    const myChart = await new Chart(ctx, {
-        type: 'bar',
+    myChart = await new Chart(ctx, {
+        type: 'line',
         data: {
             labels: xlabels,
             datasets: [{
@@ -87,10 +61,4 @@ const chartIt = async (ydata) => {
             }
           },
     });
-
-    // return myChart;
-    // myChart.destroy();
-    // document.querySelector("#chart-container").innerHTML = '<canvas id="chart"></canvas>';
 }
-// temperatures();
-// chartIt();
